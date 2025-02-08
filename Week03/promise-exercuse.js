ff738059e458228784674f90148d1336525982e0
OFF <peerapat13th@gmail.com>
2025-02-08 17:47:16 +0700

    week 3 is done.

--- Week03/promise-exercuse.js
+++ Week03/promise-exercuse.js
@@ -11,7 +11,7 @@ const getStudent = (id) => {
         }, 2000);
     });
 }
-getStudent(15).then((result) => {
+getStudent(15).then((result) => { 
     console.log(result);
 })
     .catch((err) => {
