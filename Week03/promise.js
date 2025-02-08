210fd81776edc4d7a7bf1377f850be22ae59f196
OFF <peerapat13th@gmail.com>
2025-02-08 17:00:01 +0700

    Refactor promise and callback implementations for clarity and consistency

--- Week03/promise.js
+++ Week03/promise.js
@@ -1,17 +1,18 @@
-const p = new Promise (function(resolve,reject){
-        setTimeout(function(){
-            //resolve({id:1,"name":"Suchada"});
-            reject(new Error('Error 404 Bad Request'));
-       },2000);
+//To create a promise object
+const p = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ id: 1, "name": "Suchada" });
+        reject(new Error('Error 404 Bad Request'));
+    }, 2000);
+});
+console.log('Before Asynchronous Operation');
+
+//To use the promise object
+p.then(function (result) {
+    console.log('This is result:', result);
+})
+    .catch(function (err) {
+        console.log(err.message);
     });
-    console.log('Before Asynchronous Operation');
-    
-    p.then(function(result){
-        console.log('This is result: ',result);
-    })
-    .catch(function(err){
-        console.log(err.message);
-    });
-    
-    console.log('After Asynchronous Operation');
-    
+
+console.log('After Asynchronous Operation');
--- Week03/promiseall.js
+++ Week03/promiseall.js
@@ -1,17 +1,21 @@
-const myPromise1 = new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log('First');
-            resolve(1);
-        },3000);
-    });
-const myPromise2 = new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log('Second');
-            resolve(2);
-        },2000);
-    });
-Promise.all([myPromise1,myPromise2])
-        .then((results)=>{
-        console.log(results);
+//To create a promise object
+const myPromise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('First');
+        resolve(1);
+    }, 3000);
 });
-    
+
+//To create a promise object
+const myPromise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Second');
+        resolve(2);
+    }, 2000);
+});
+
+//to use the promise object
+Promise.all([myPromise1, myPromise2])
+    .then((results) => {
+        console.log(results);
+    });
